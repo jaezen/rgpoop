3338609602ba00183d0f10276d0a3ea5cf8bb7bc
jaezen <jaezenlim@gmail.com>
2016-07-03 14:43:05 -0700

    Initial Commit

---
+++ rgpoop/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  rgpoop
+//
+//  Created by Jaezen Lim on 3/07/2016.
+//  Copyright © 2016 jaezenlim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
